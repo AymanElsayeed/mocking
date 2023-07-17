ebed881c076906a565cb784cc3682f0a38efd4d6
Yarden7696 <yarden7696@gmail.com>
2023-07-17 19:36:42 +0300

    add docstring

--- src/inht_abc.py
+++ src/inht_abc.py
@@ -34,7 +34,13 @@ class B(A):
     def __init__(self, n):
         super().__init__(n)
 
-    def add(self, n):
+    def add(self, n) -> int:
+        """
+        add n to self.n
+        :param n: n
+        :return: n plus self.n
+        :rtype: int
+        """
         self.n += n
 
 
